2d06474e3e75643a48f6be2c490d6b2935d6926c
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-30 16:10:58 +0000

    Finished guessing game gui

--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+
+  JLabel prompt;
+  JLabel feedback;
+  JLabel answer;
+
+  JTextField userGuess;
+
+  JButton submit;
+  JButton newNumber;
 
 
   // Method to assemble our GUI
@@ -18,15 +28,98 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
+
+    //initialize main panel
+    mainPanel = new JPanel();
+    //remove layout helpers
+    mainPanel.setLayout(null);
+
+    //starts method that selects random number
+    randSelector();
+
+    //initialize labels
+    prompt = new JLabel("Guess a number between 0 and 100");
+    feedback = new JLabel();
+
+    //sets location and size of labels
+    prompt.setBounds(250, 20, 300, 20);
+    feedback.setBounds(325, 120, 300, 20);
+
+    //initialize text field
+    userGuess = new JTextField();
+
+    //sets location for user text field
+    userGuess.setBounds(225, 60, 300, 20);
+
+    //initialize buttons
+    submit = new JButton("Submit");
+    newNumber = new JButton("New Number");
+
+    //sets location and size of buttons
+    submit.setBounds(225, 80, 120, 40);
+    newNumber.setBounds(355, 80, 170, 40);
+
+    //adds action listener and action command to buttons
+    submit.addActionListener(this);
+    newNumber.addActionListener(this);
+
+    submit.setActionCommand("submit");
+    newNumber.setActionCommand("numNew");
+
+    //adds everything to main panel 
+    mainPanel.add(prompt);
+    mainPanel.add(feedback);
+    mainPanel.add(submit);
+    mainPanel.add(newNumber);
+    mainPanel.add(userGuess);
+
+    //adds main panel to frame
+    frame.add(mainPanel);
  
     
-
   }
+
+  //method that sets a random number
+  public void randSelector(){
+    //creates random number
+    Random rand = new Random();
+    int randNum = rand.nextInt(100) + 0;
+
+    //initialize label for random number
+    answer = new JLabel("" + randNum);
+
+    //sets the answer so that the user cannot see it
+    answer.setBounds(0, 0, 0, 0);
+  }
+
 
   // method called when a button is pressed
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+    //gets number and rand num
+    String userString = userGuess.getText();
+    String randString = answer.getText();
+
+    //converts numbers to int
+    int userNum = Integer.parseInt(userString);
+    int randNum = Integer.parseInt(randString);
+
+    if(command.equals("submit")){
+      if(userNum == randNum){
+        feedback.setText(userNum + " is the right answer!");
+        randSelector();
+      }else if(userNum > randNum){
+        feedback.setText(userNum + " is too high!");
+      }else if(userNum < randNum){
+        feedback.setText(userNum + " is too low!");
+      }
+
+    }else if(command.equals("numNew")){
+      randSelector();
+      userGuess.setText("");
+      feedback.setText("");
+    }
 
   }
 
